a3db1c07d1635631650ee6243285cf94e4e53312
yujimin413 <84549242+yujimin413@users.noreply.github.com>
2026-03-04 11:26:28 +0900

    [FIX] components 폴더 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import AppShell from "./components/layouts/app-shell";
+import AppShell from "../components/layouts/app-shell";
 
 export default function Home() {
   return (
     <AppShell>
       <div className="breadcrumb">홈 / 식당 리스트 / 상세</div>
-      <h1 className="page-title">식당 상세</h1>
     </AppShell>
   );
 }
